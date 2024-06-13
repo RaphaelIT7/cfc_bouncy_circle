ae7bce95989bfc9ebe3063a042d66e989d0d3f7c
RaphaelIT7 <raphael.i.t@outlook.de>
2024-06-13 09:19:06 +0200

    Fix prediction for players.
    This works well, but if the trampoline is angled, one prediction error will occur and the player will get seemingly teleported clientside.

--- lua/entities/cfc_trampoline/cl_init.lua
+++ lua/entities/cfc_trampoline/cl_init.lua
@@ -5,10 +5,13 @@ function ENT:Draw()
 end
 
 local BOUNCE_SFX_VOLUME = CreateClientConVar( "cfc_trampoline_volume", 1, true, false, "Volume for the trampoline", 0, 1 )
+function ENT:bounceSound()
+	self:EmitSound( "cfc/cfc_trampoline/cfc_trampoline_bounce.ogg", 75, 100, BOUNCE_SFX_VOLUME:GetFloat() )
+end
 
 net.Receive( "CFC_BouncyCircle_PlayBounceSound", function()
     local ent = net.ReadEntity()
     if not IsValid( ent ) then return end
 
-    ent:EmitSound( "cfc/cfc_trampoline/cfc_trampoline_bounce.ogg", 75, 100, BOUNCE_SFX_VOLUME:GetFloat() )
+    ent:bounceSound()
 end )
